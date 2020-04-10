a544789c8651fcf77a7c1f35e50046061c18c69d
Jason McCoy <jmccoy@hashicorp.com>
2020-04-10 13:08:01 -0400

    initial commit

---
+++ main.tf
@@ -0,0 +1,47 @@
+provider "tfe" {
+  token = var.token
+}
+
+resource "tfe_workspace" "main" {
+  name              = var.workspace_name
+  organization      = var.organization
+  auto_apply        = "true"
+  queue_all_runs    = "true"
+  terraform_version = "0.12.3"
+}
+
+resource "tfe_variable" "tfvars_sensitive" {
+  count        = length(var.tfe_vars_sensitive)
+  key          = element(keys(var.tfe_vars_sensitive), count.index)
+  value        = element(values(var.tfe_vars_sensitive), count.index)
+  category     = "terraform"
+  sensitive    = true
+  workspace_id = tfe_workspace.main.id
+}
+
+resource "tfe_variable" "env_vars_sensitive" {
+  count        = length(var.env_vars_sensitive)
+  key          = element(keys(var.env_vars_sensitive), count.index)
+  value        = element(values(var.env_vars_sensitive), count.index)
+  category     = "env"
+  sensitive    = true
+  workspace_id = tfe_workspace.main.id
+}
+
+resource "tfe_variable" "tfvars_nonsensitive" {
+  count        = length(var.tfe_vars_nonsensitive)
+  key          = element(keys(var.tfe_vars_nonsensitive), count.index)
+  value        = element(values(var.tfe_vars_nonsensitive), count.index)
+  category     = "terraform"
+  sensitive    = false
+  workspace_id = tfe_workspace.main.id
+}
+
+resource "tfe_variable" "env_vars_nonsensitive" {
+  count        = length(var.env_vars_nonsensitive)
+  key          = element(keys(var.env_vars_nonsensitive), count.index)
+  value        = element(values(var.env_vars_nonsensitive), count.index)
+  category     = "env"
+  sensitive    = false
+  workspace_id = tfe_workspace.main.id
+}
---
+++ terraform.tfvars
@@ -0,0 +1,33 @@
+# THESE VALUES CAN BE MANAGED WITHIN THE WORKSPACE ITSELF
+# The Values are unchanging in the 'Master' Workspace
+
+token = "Terraform Enterprise API token"
+
+organization = "hc-solutions-engineering"
+
+vcs_repo = "VCS/Repo"
+
+oauth_token_id = "oauth_token_id"
+
+
+# The Values can be changed for use in the newly-created Workspace
+# Additional values could be added here (ex. branch, etc.)
+
+workspace_name = "name of workspace to create"
+
+
+tfe_vars_sensitive = {
+    foo = "bar"
+}
+
+env_vars_sensitive  = {
+    Bob = "Alice"
+}
+
+tfe_vars_nonsensitive = {
+    foo = "bar"
+}
+
+env_vars_nonsensitive  = {
+    Bob = "Alice"
+}
